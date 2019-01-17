f13a56ee016dc3fc1b6247bfc950cc3020a8cdd0
James Joseph (CHomes) <jaayjay@gmail.com>
2019-01-17 13:55:53 +0000

    Starting add_host py defining functions

---
+++ python_scripts/add_host.py
@@ -0,0 +1,18 @@
+#author: chomes@github
+#Version: 1.0
+#Future version: Adding argparse as a one line command rather then using inputs all the time.
+
+def config_startup():
+
+
+def conf_create_linux():
+
+
+def conf_create_windows():
+
+
+def template_windows(host, hostip, shortname):
+
+
+def template_linux(host, hostip, shortname):
+
